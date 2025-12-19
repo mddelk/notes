09ebbbc8e48305d4c9644b351f206d3546efcf45
Mark Delk <jethrodaniel@gmail.com>
2025-12-19 01:19:48 -0600

    speed up test:system in ci

--- config/ci.rb
+++ config/ci.rb
@@ -13,6 +13,6 @@ CI.run "bin/ci", "Make it so" do
     "bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error"
 
   step "Tests: Rails", "BROWSER=rack-test bin/rails test:all"
-  step "Tests: System", "bin/rails test:system"
+  step "Tests: System", "PARALLEL_WORKERS=4 bin/rails test:system"
   step "Tests: Seeds", "env RAILS_ENV=test bin/rails db:seed:replant"
 end
