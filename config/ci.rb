3a5ccc58c4af8672c3c41198db950261f36da8c8
Mark Delk <24943868+mddelk@users.noreply.github.com>
2026-05-09 14:15:22 -0500

    fix bin/ci test speed

--- config/ci.rb
+++ config/ci.rb
@@ -15,6 +15,6 @@ CI.run "bin/ci", "Make it so" do
     "bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error"
 
   step "Tests: Rails", "BROWSER=rack-test bin/rails test:all"
-  step "Tests: System", "PARALLEL_WORKERS=4 bin/rails test:system"
+  step "Tests: System", "bin/rails test:system"
   step "Tests: Seeds", "RAILS_ENV=test bin/rails db:seed:replant"
 end
